616eedd68ac65dda1c87c444aa85d24abbf0d564
apjanco <apjanco@gmail.com>
2022-11-15 15:22:37 -0500

    getting to work on Netlify

---
+++ .eleventy.js
@@ -0,0 +1,14 @@
+module.exports = function(eleventyConfig) {
+    // Output directory: _site
+  
+    // Copy `assets/` to `_site/assets`
+    eleventyConfig.addPassthroughCopy("assets");
+    
+    // Copy `css/fonts/` to `_site/css/fonts`
+    // Keeps the same directory structure.
+    //eleventyConfig.addPassthroughCopy("css/fonts");
+  
+    // Copy any .jpg file to `_site`, via Glob pattern
+    // Keeps the same directory structure.
+    //eleventyConfig.addPassthroughCopy("umpire/*.jpg");
+  };
