31f491a8a42fc8fa632b4502f5d6cb914ae24b33
Whitney Trettien <wtrettien@gmail.com>
2024-08-13 12:19:34 -0400

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -20,6 +20,25 @@ eleventyConfig.addCollection("gossipAdviceJokes", function (collection) {
 
 });
 
+eleventyConfig.addFilter("dateOnly", function (dateVal, locale = "en-us") {
+  var theDate = new Date(dateVal);
+  const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+  return theDate.toLocaleDateString(locale, options);
+});
+
+	// Return all the tags used in a collection
+	eleventyConfig.addFilter("getAllTags", collection => {
+		let tagSet = new Set();
+		for(let item of collection) {
+			(item.data.tags || []).forEach(tag => tagSet.add(tag));
+		}
+		return Array.from(tagSet);
+	});
+
+	eleventyConfig.addFilter("filterTagList", function filterTagList(tags) {
+		return (tags || []).filter(tag => ["all", "nav", "post", "posts"].indexOf(tag) === -1);
+	});
+
     // Copy `css/fonts/` to `_site/css/fonts`
     // Keeps the same directory structure.
     //eleventyConfig.addPassthroughCopy("css/fonts");
